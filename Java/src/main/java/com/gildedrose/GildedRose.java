b5947a1b8d1df40652b44e8a90914e3f8806f28a
Harald Reingruber <harald.reingruber@dedalus-group.com>
2021-03-26 11:00:03 +0100

    Moved itemQuality method to Item class

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -22,54 +22,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < LOW_QUALITY_GATE) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < LOW_QUALITY_GATE) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < LOW_QUALITY_GATE) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < LOW_QUALITY_GATE) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            items[i].updateItemQuality();
         }
     }
+
 }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,55 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+   public void updateItemQuality() {
+        if (!name.equals(GildedRose.AGED_BRIE)
+                && !name.equals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            if (quality > 0) {
+                if (!name.equals(GildedRose.SULFURAS_HAND_OF_RAGNAROS)) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < GildedRose.LOW_QUALITY_GATE) {
+                quality = quality + 1;
+
+                if (name.equals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                    if (sellIn < 11) {
+                        if (quality < GildedRose.LOW_QUALITY_GATE) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < GildedRose.LOW_QUALITY_GATE) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!name.equals(GildedRose.SULFURAS_HAND_OF_RAGNAROS)) {
+            sellIn = sellIn - 1;
+        }
+
+        if (sellIn < 0) {
+            if (!name.equals(GildedRose.AGED_BRIE)) {
+                if (!name.equals(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                    if (quality > 0) {
+                        if (!name.equals(GildedRose.SULFURAS_HAND_OF_RAGNAROS)) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = quality - quality;
+                }
+            } else {
+                if (quality < GildedRose.LOW_QUALITY_GATE) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
 }
