87b66fc87aa56925ebb2ac2929fa7625ec45c51e
Harald Reingruber <harald.reingruber@dedalus-group.com>
2021-04-09 11:30:26 +0200

    Modifying IF statements

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -73,10 +73,8 @@ public class Item {
                     quality++;
                 }
 
-                if (sellIn < 6) {
-                    if (quality < LOW_QUALITY_GATE) {
-                        quality++;
-                    }
+                if (sellIn < 6 && quality < LOW_QUALITY_GATE) {
+                    quality++;
                 }
             }
         }
